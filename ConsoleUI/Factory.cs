5af57536ec4122c9102404087e87f9787260c979
youcef baghdadi <youssef.xd.12@gmail.com>
2019-06-10 02:45:06 +0100

    first

---
+++ ConsoleUI/Factory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    public static class Factory
+    {
+        public static IPerson CreatePerson()
+        {
+            return new Person();
+        }
+
+        public static IChore CreateChore()
+        {
+            return new Chore(CreateLogger(),CreateMessageSender());
+        }
+
+        public static ILogger CreateLogger()
+        {
+            return new Logger();
+        }
+
+        public static IMessageSender CreateMessageSender()
+        {
+            return new  Texter();
+        }
+    }
+}
